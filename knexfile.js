827e6106bdbd94bd90dbe1e47b171f5d34f340f5
wendyhoy <wendy.hoy@gmail.com>
2018-03-12 18:00:01 -0700

    Try to set up production database with DATABASE_URL

--- knexfile.js
+++ knexfile.js
@@ -29,9 +29,7 @@ module.exports = {
 
   production: {
     ...sharedConfig,
-    connection: {
-      database: 'postgres://ihegqtrysvrzzv:8f1018e91794bbec96514fa4f5cf521fb2023ed62c427f88a5b99a72da5cf8b6@ec2-54-83-23-91.compute-1.amazonaws.com:5432/d1hqvhbatltth6'
-    }
+    connection: process.env.DATABASE_URL
   }
 
 };
